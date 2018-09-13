368c8381b36b706a45d17bbb575fe8c9f1a61a80
TsukiyomiMoonscar <joey.vandewaal@student.hu.nl>
2018-09-13 11:22:02 +0200

    ltth

--- Les 4/PE 4_1.py
+++ Les 4/PE 4_1.py
@@ -3,4 +3,4 @@ cijferPROG=(7)
 cijferCSN=(7.5)
 gemiddelde=round(((cijferCSN + cijferICOR + cijferPROG)/3),1)
 Overzicht=('Het gemiddelde van de drie cursusen is {}\nHet bedrag wat de HU je schenkt is €{},-'.format(gemiddelde, round(gemiddelde*30)))
-print(Overzicht)
+print(Overzicht)    gdsg
